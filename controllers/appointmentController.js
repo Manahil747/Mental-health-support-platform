c2e217fd667fa1cd14247fb5ec5d2a24e19a0130
Manahil Altaf <mnhl3755@gmail.com>
2026-04-25 17:32:45 +0500

    Email notifications added

--- controllers/appointmentController.js
+++ controllers/appointmentController.js
@@ -1,5 +1,8 @@
 const Appointment=require('../models/appointment');
 const responseModel=require("../utils/responseModel");
+const User= require("../models/user");
+const Therapist=require("../models/therapist");
+const sendEmail= require("../utils/sendEmail");
 
 //Book appointment
 const bookAppointment=async(req,res)=>{
@@ -7,6 +10,15 @@ const bookAppointment=async(req,res)=>{
         const {therapistId, date, time}= req.body;
         const newAppointment= new Appointment({userId:req.user.userId, therapistId, date, time});
         await newAppointment.save();
+        const user = await User.findById(req.user.userId);
+        const therapist = await Therapist.findById(therapistId);
+        // send email to user
+        await sendEmail(
+            user.email,
+            'Appointment Booked Successfully!',
+            `Hello ${user.name}! Your appointment with ${therapist.name} is booked on ${date} at ${time}.`
+        );
+
         return res.status(201).json(responseModel({statusCode:201, success:true, data:newAppointment ,message:'Appointment booked sucessfully:)'}))
     }
     catch(err){
@@ -34,7 +46,7 @@ const cancelAppointment=async(req,res)=>{
     res.status(200).json(responseModel({statusCode:200, success:true, data:appointmentCancel, message:'Appointment cancel successfully:)'}));
     }
     catch(err){
-        res.status(400).json(responseModel({statusCode:400, success:false, message:'Not cancelled:('}));
+        res.status(400).json(responseModel({statusCode:400, success:false, message:'Not cancelled:(',err:err.message}));
     }
 }
 module.exports={bookAppointment, getUserAppointment, cancelAppointment};
